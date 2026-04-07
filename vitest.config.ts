7bf48a43887c4cf58c10049d39be71cb9def2cff
jason.dimeo <jason.dimeo@gmail.com>
2026-04-07 18:38:21 -0400

    refactor!: upgrade to TypeScript 6
    Modifies the TypeScript compiler options to use module preserve and enables consistent structure and sourcemaps.
    Adds an alias for the src directory in the Vitest configuration and disables redundant typechecking during unit tests.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+	resolve: {
+    alias: { 'src': new URL('./src', import.meta.url).pathname }
+  },
 	test: {
 		environment: 'node',
+		typecheck: { enabled: false },
 		coverage: {
 			reportsDirectory: './tests/coverage',
 			include: ['src/**/*.ts'],
